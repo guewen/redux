e31e3661183fdceb73c426756cbfb1e7971eec02
Guewen Baconnier <guewen@gmail.com>
2012-07-25 22:55:29 +0200

    removed test

--- test_tasks/tasks.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-celery.config_from_object('celeryconfig')
-celery = Celery('tasks', backend='amqp', broker='amqp://guest@localhost//')
-
-@celery.task
-def add(x, y):
-    return x + y
-
